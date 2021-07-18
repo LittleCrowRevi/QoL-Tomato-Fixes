7a638b58b2d50a24caf542774f2c0111fba30dd8
Revi <snowrevy@gmail.com>
2021-07-18 13:59:54 +0200

    Adds a command for breaking snow in a chunk-sized radius around the player on one height to save resources.

--- src/main/java/littlecrow/commands/thawSnowCmd.java
+++ src/main/java/littlecrow/commands/thawSnowCmd.java
@@ -4,13 +4,20 @@ import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.SnowBlock;
 import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.entity.Entity;
+import net.minecraft.network.MessageType;
 import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.text.LiteralText;
+import net.minecraft.util.Formatting;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.world.World;
+
+import java.util.Objects;
 
 public class thawSnowCmd {
 
@@ -19,19 +26,24 @@ public class thawSnowCmd {
         ServerCommandSource source = ctx.getSource();
         Entity player = source.getPlayer();
         World world = source.getWorld();
+        int snowCount = 0;
         for (long x = (long) player.getX() - 8; x <= player.getX() + 8; x++) {
-            for (long y = 50; y <= 80; y++) {
-                for (long z = (long) (player.getZ() - 8); z <= player.getZ() + 8; z++) {
-                    Block snow = Registry.BLOCK.get(new Identifier("minecraft:snow"));
-                    BlockState b = world.getBlockState(BlockPos.fromLong(x + y + z));
-                   if (b.getBlock().is(snow)) {
-                       world.breakBlock(BlockPos.fromLong(x + y + z), false);
-                   }
-                }
-            }
+             for (long z = (long) (player.getZ() - 8); z <= player.getZ() + 8; z++) {
+                 BlockPos pos = new BlockPos(x, player.getY(), z);
+                 System.out.println(pos);
+                 Block snow = Registry.BLOCK.get(new Identifier("minecraft", "snow"));
+                 System.out.println(snow);
+                 Block e = world.getBlockState(pos).getBlock();
+                 System.out.println(e);
+                 if (e != null && e.is(snow)) {
+                     snowCount++;
+                     world.breakBlock(pos, false);
+                 }
+             }
         }
-
-
+        source.getMinecraftServer()
+                .getPlayerManager()
+                .broadcastChatMessage(new LiteralText("removed " + snowCount + " snow.").formatted(Formatting.BLUE), MessageType.SYSTEM, player.getUuid());
         return 1;
     }
 }
